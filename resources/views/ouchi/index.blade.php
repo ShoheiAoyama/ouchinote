2d49a9fe6109d907df662271ca9889549e5eff79
ShoheiAoyama <19941121as@gmail.com>
2020-12-29 01:50:01 +0900

    organize

--- resources/views/ouchi/index.blade.php
+++ resources/views/ouchi/index.blade.php
@@ -23,7 +23,7 @@
                                 <th scope="col" style="text-align: center;">COMPANY</th>
                                 <th scope="col" style="text-align: center;">WEB</th>
                                 <th scope="col" style="text-align: center;">FIN</th>
-                                {{--                                <th scope="col">EMAIL</th>--}}
+                                {{--<th scope="col">EMAIL</th>--}}
                                 <th scope="col" style="text-align: center;">SNS</th>
                                 <th scope="col" style="text-align: center;">INFO</th>
                             </tr>
@@ -34,7 +34,7 @@
                                     <th>{{$contact->company_name }}</th>
                                     <td><a href="{{$contact->url}}">web</a></td>
                                     <td>{{$contact->financing_plan}}万円</td>
-                                    {{--                                    <td><a href="{{$contact->company_email}}">email</a></td>--}}
+                                    {{--<td><a href="{{$contact->company_email}}">email</a></td>--}}
                                     <td><a href="{{$contact->sns}}">sns</a></td>
                                     <td><a href="{{ route('ouchi.show',['id'=>$contact->id])}}">info</a></td>
                                 </tr>
